5d2f8dcd161af0f0f37f8d8673ec6d636e9214a6
Daniel Carbajal Fernandez <dcarbajalfernandez@gmail.com>
2023-11-03 11:05:58 -0500

    Sorting

--- DataStructures/DataStructures/Program.cs
+++ DataStructures/DataStructures/Program.cs
@@ -1,6 +1,12 @@
 ﻿using DataStructures;
+using System.Linq;
 
 int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
 var result = BinarySearch.SearchIterable(array, 3);
 Console.WriteLine(result);
+
+int[] array2 = { 5, 2, 3, 8, 9, 0, 1, 4, 6, 7, 10 };
+var result2 = Sorting.BubbleSort(array2);
+var resp = string.Join(", ", result2);
+Console.WriteLine(resp);
 Console.ReadKey();
---
+++ DataStructures/DataStructures/Sorting.cs
@@ -0,0 +1,28 @@
+﻿namespace DataStructures
+{
+    public class Sorting
+    {
+        public static int[] BubbleSort(int[] array)
+        {
+            for (int wall = array.Length - 1; wall >= 0; wall--)
+            {
+                for (int i = 0; i < wall; i++)
+                {
+                    if (array[i] > array[i + 1])
+                        Swap(array, i, i + 1);
+                }
+            }
+
+            return array;
+        }
+
+        private static void Swap(int[] array, int leftIndex, int rightIndex)
+        {
+            if (leftIndex == rightIndex) return;
+
+            var temp = array[rightIndex];
+            array[rightIndex] = array[leftIndex];
+            array[leftIndex] = temp;
+        }
+    }
+}
